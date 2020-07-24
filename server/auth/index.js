cb2c52bbd497668f46df5e9e5398ad096f44587d
Emmanuel Ochoa <eorivera95@gmail.com>
2020-07-24 16:32:42 -0600

    remove hashed password from the signup response

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,13 +1,47 @@
 const express = require("express");
 const router = express.Router();
+const Joi = require("@hapi/joi");
+const bcrypt = require("bcrypt");
+
+const db = require("../db/connection");
+const users = db.get("users");
+users.createIndex("username", { unique: true });
+
+const schema = Joi.object({
+  username: Joi.string().pattern(new RegExp("^[a-zA-Z0-9_]{2,30}$")).required(),
+
+  password: Joi.string().trim().min(6).required(),
+});
 
 router.get("/", (req, res) => {
   res.json({ message: "GET auth" });
 });
 
-router.post("/signup", (req, res) => {
-  console.log(req.body.username);
-  res.json({ message: "POST auth/signup" });
+router.post("/signup", async (req, res, next) => {
+  try {
+    const user = await schema.validateAsync(req.body);
+    const userExist = await users.findOne({ username: user.username });
+
+    if (userExist) {
+      const error = new Error("The user already exists");
+      next(error);
+    } else {
+      //Hash password
+      const salt = await bcrypt.genSalt();
+      const hashPassword = await bcrypt.hash(req.body.password, salt);
+
+      const insertedUser = await users.insert({
+        username: req.body.username,
+        password: hashPassword,
+      });
+      delete insertedUser.password;
+      res.json(insertedUser);
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(400);
+    next(error);
+  }
 });
 
 module.exports = router;
--- server/index.js
+++ server/index.js
@@ -3,13 +3,28 @@ const morgan = require("morgan");
 
 const app = express();
 
-const auth = require("./auth/index");
+const auth = require("./auth");
 
 app.use(morgan("dev"));
 
-express.json();
+app.use(express.json());
+
 app.use("/auth", auth);
 
+function notFound(req, res, next) {
+  res.status(404);
+  const error = new Error("Not Found -" + req.originalUrl);
+  next(error);
+}
+function errorHandler(err, req, res, next) {
+  res.status(res.statusCode || 500);
+  res.json({
+    message: err.message,
+    stack: err.stack,
+  });
+}
+app.use(notFound);
+app.use(errorHandler);
 const PORT = 3000;
 
 app.listen(PORT, () => console.log("Server running"));
